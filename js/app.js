7903edb0bd55c5dc560d1ac51b3bebbfd70cfeea
Coutinho, Reuben <reuben.coutinho@accenture.com>
2017-10-25 17:03:51 +1100

    Feature: added functionality to restart game and open tile

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 /*
  * Create a list that holds all of your cards
  */
-
+const lookup =["fa-diamond","fa-paper-plane-o","fa-anchor","fa-bolt","fa-cube","fa-leaf","fa-bicycle","fa-bomb","fa-diamond","fa-paper-plane-o","fa-anchor","fa-bolt","fa-cube","fa-leaf","fa-bicycle","fa-bomb"];
 
 /*
  * Display the cards on the page
@@ -10,7 +10,11 @@
  *   - add each card's HTML to the page
  */
 
-// Shuffle function from http://stackoverflow.com/a/2450976
+/**
+* @description Shuffles an array
+* @param {array} array - the array that needs to be shuffled
+* @@returns {array} - The shuffled array
+*/
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
 
@@ -25,6 +29,58 @@ function shuffle(array) {
     return array;
 }
 
+/**
+* @description Initialized the game
+*/
+function initialize(){
+    resetCard();
+    var shuffledArray = shuffle(lookup);
+    var cards = $("li.card");
+    shuffledArray.forEach((value,index)=>{
+        $(cards[index]).children(".fa").addClass(value);
+    }, this);
+
+}
+
+/**
+* @description clear any existing class on card
+*/
+function resetCard(){
+    var cards = $("li.card");
+    for(var i =0; i<cards.length;i++){
+        $(cards[i]).children(".fa").removeClass(function(index, className) {
+            console.log(index+" "+className)
+            //return $( this ).prev().attr( "class" );
+            $( this ).removeClass(className);
+            $( this ).addClass("fa");
+          })
+
+          $(cards[i]).removeClass("show")
+          $(cards[i]).removeClass("open")
+    }
+}
+
+
+$(()=>{
+    $("ul.deck").hide();
+    $(".restart").click((e)=>{
+        $("ul.deck").hide();
+        $(".info-panel").slideUp(1000);
+        initialize();
+        setTimeout(()=>{
+            $("ul.deck").slideDown(1000);
+        },1000)
+    })
+
+    $("li.card").click((e)=>{
+        $(e.target).toggleClass("show").toggleClass("open")
+    })
+
+    $("li.card>i.fa").click((e)=>{
+        $(e.target).parent().toggleClass("show").toggleClass("open")
+    })
+
+});
 
 /*
  * set up the event listener for a card. If a card is clicked:
@@ -36,3 +92,4 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
