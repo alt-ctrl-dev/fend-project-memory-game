98b79c27e909245540b090f18db2e193f94be40f
Coutinho, Reuben <reuben.coutinho@accenture.com>
2017-10-26 09:10:25 +1100

    Fix: removed toggle and adding class to prevent closing tile if already open

--- js/app.js
+++ js/app.js
@@ -1,14 +1,7 @@
 /*
- * Create a list that holds all of your cards
+ * Create an array of card faces
  */
 const lookup =["fa-diamond","fa-paper-plane-o","fa-anchor","fa-bolt","fa-cube","fa-leaf","fa-bicycle","fa-bomb","fa-diamond","fa-paper-plane-o","fa-anchor","fa-bolt","fa-cube","fa-leaf","fa-bicycle","fa-bomb"];
-
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
 
 /**
 * @description Shuffles an array
@@ -96,27 +89,15 @@ $(()=>{
 
     //Wiring click on tile to open/close tile
     $("li.card").click((e)=>{
-        if($(e.target).hasClass("match"))return;
-        $(e.target).toggleClass("show").toggleClass("open");
+        //if($(e.target).hasClass("match") || $(e.target).hasClass("open"))e.preventDefault();
+        $(e.target).addClass("show").addClass("open");
         checkGame();
     });
 
     //Wiring click on tile icon to open/close tile
     $("li.card>i.fa").click((e)=>{
-        if($(e.target).parent().hasClass("match"))return;
-        $(e.target).parent().toggleClass("show").toggleClass("open");
+        //if($(e.target).parent().hasClass("match") || $(e.target).parent().hasClass("open"))e.preventDefault();
+        $(e.target).parent().addClass("show").addClass("open");
         checkGame();
     });
 });
-
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
-
